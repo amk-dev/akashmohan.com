845f07f0de58d4124a860df295722f36241a5b5b
amk-dev <akash.k.mohan98@gmail.com>
2021-04-05 06:39:35 +0530

    Add icons for all platforms

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,31 @@ export default {
 			},
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png',
+			},
+			{ rel: 'manifest', href: '/site.webmanifest' },
+			{
+				rel: 'mask-icon',
+				href: '/safari-pinned-tab.svg',
+				color: '#5bbad5',
+			},
+			{ name: 'msapplication-TileColor', content: '#2d89ef' },
+			{ name: 'theme-color', content: '#ffffff' },
 			{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
 			{
 				rel: 'stylesheet',
